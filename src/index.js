7742a6ade94a02769ef33ab28773a8986439c291
angeldamcheski <damcheskiangel911@gmail.com>
2024-01-06 15:32:54 +0100

    Minor changes to app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pasth = require("path");
+const path = require("path");
 const bcrypt = require("bcrypt");
 const { collection, posts } = require("./config");
 const session = require("express-session");
